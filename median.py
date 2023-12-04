3347aaeb6b9c6747e891e08a2c47c00a0097b1bd
Sujan Naik <sujannaik047@gmail.com>
2023-12-04 12:06:38 +0000

    Finish exercise

--- median.py
+++ median.py
@@ -9,4 +9,13 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
+numbers.sort()
+if len(numbers) % 2 == 0:
+    first_index = len(numbers) / 2 - 1
+    second_index = len(numbers) / 2
+    median = (numbers[int(first_index)] + numbers[int(second_index)])/2
+    print(median)
+else:
+    index = (len(numbers) - 1) / 2
+    print(numbers[int(index)])
